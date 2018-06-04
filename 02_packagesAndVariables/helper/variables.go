0b6e73d4d4903f8df9a60978b3df81d2c52ea633
Edib Imamovic <edib.imamovic@gmail.com>
2018-06-04 16:04:40 +0200

    Added second exercise with exercise for variables , scope and packages

---
+++ 02_packagesAndVariables/helper/variables.go
@@ -0,0 +1,14 @@
+package helper
+
+func Variables() {
+
+	foo()
+
+}
+func foo() {
+	c := "Some string"
+	var d = 48
+
+	println(c)
+	println(d)
+}
---
+++ 02_packagesAndVariables/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/edibImamovic/GoExercises/02_packagesAndVariables/helper"
+)
+
+func main() {
+
+	fmt.Println(helper.Name)
+	helper.Variables()
+
+}
